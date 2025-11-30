f1ad7d11e3d31f2213856b011a88be4cdc4062a4
Alex_Wondermaker <papajohn.strike@gmail.com>
2025-11-30 19:14:11 +0100

    try to change position clue candy for mobile

--- src/components/Info/CandyBlue/CandyBlue.jsx
+++ src/components/Info/CandyBlue/CandyBlue.jsx
@@ -3,7 +3,7 @@ export default function CandyBlue() {
     <img
       src="images/candyBlue.png"
       alt="Blue candy"
-      className="absolute bottom-[62px] md:bottom-0 w-[170px]  md:w-[190px] md:right-0 sm:bottom-0 sm:w-[150px] sm:right-0"
+      className="absolute bottom-0 md:bottom-0 w-[170px]  md:w-[190px] md:right-0  sm:w-[150px] sm:right-0"
     />
   );
 }
