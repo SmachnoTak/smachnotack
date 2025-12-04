5a7a1296c7bd653903f8ccf02b0477c3a082ed8e
Alex_Wondermaker <papajohn.strike@gmail.com>
2025-12-04 15:08:20 +0100

    again changed 'phone' position and added shadows

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -7,7 +7,7 @@ import Phone from "./ContactPart/Phone/Phone";
 
 export default function Info() {
   return (
-    <div className=" bg-white w-[92%]  flex justify-center items-center rounded-4xl">
+    <div className=" bg-white w-[92%]  flex justify-center items-center rounded-4xl shadow-[0_0_15px_rgba(0,0,0,0.3)]">
       <Background />
       <Label />
       <ContactPart />
