12f917d19a1ebdbc8f623d22c0433d1c47b9b2d2
Alex_Wondermaker <papajohn.strike@gmail.com>
2025-12-10 13:24:13 +0100

    added label into head

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <main className="relative text-center">
       <div
-        className="  min-w-screen min-h-screen flex  justify-center pt-[12%]! pb-[8%]! xl:py-20! md:pb-12! md:pt-10! sm:pt-[5%]! sm:pb-2!"
+        className="  min-w-screen min-h-screen flex  justify-center pt-[12%]! pb-[8%]! xl:pt-20! xl:pb-12! md:pb-12! md:pt-10! sm:pt-[5%]! sm:pb-2!"
         style={{ background: "#69C3FA" }}
       >
         <Info />
       </div>
       <p
-        className="  font-[Raleway] pb-5! text-sm xl:bottom-[10%] md:absolute md:bottom-0 md:left-1/2 md:translate-x-[-50%] md:bg-transparent!  "
+        className="  font-[Raleway] pb-5! text-sm xl:bottom-[6%] md:absolute md:bottom-0 md:left-1/2 md:translate-x-[-50%] md:bg-transparent!  "
         style={{ color: "#323030", background: "#69C3FA" }}
       >
         © 2025 ТМ СмачноТак!
